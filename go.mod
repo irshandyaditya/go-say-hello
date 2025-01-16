6f1938465dfdd813e3c8e787b9b5a669a298a7b7
Irshandy Aditya <irshandy.aditya@gmail.com>
2025-01-16 09:41:23 +0700

    update SayHello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
-module github.com/irshandyaditya/go-say-hello
+module github.com/irshandyaditya/go-say-hello/v2
 
 go 1.23.4
+
+// major changes lebih baik mengubah nama module agar ketika go get dilakukan,
+// pemakainya tidak mengalami error pada semua penggunaan yang mengalami perubahan
--- say_hello.go
+++ say_hello.go
@@ -1,6 +1,9 @@
 package gosayhello
 
 // membuat module dan up ke github
-func SayHello() string {
-	return "Hellooww World's people"
+func SayHello(name string) string {
+	return "Heyyy, " + name
 }
+
+// major changes lebih baik mengubah nama module agar ketika go get dilakukan,
+// pemakainya tidak mengalami error pada semua penggunaan yang mengalami perubahan
